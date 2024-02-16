b21dd597225951403b30f50615041ce37fe046d5
1imo <100065732+1imo@users.noreply.github.com>
2024-02-16 20:56:32 +0000

    Last Commit
    Issue fixed where slider wasn't snapping to the end before callback. Issue fixed where the slider was not resetting on state change. Fully functioning now but still getting the odd glitch now and then. Passable for my current use-case

--- Slider.jsx
+++ Slider.jsx
@@ -1,118 +1,133 @@
 import { useEffect, useState } from "react";
 
 function Slider(props) {
-  const [ x, setX ] = useState(0)
-  const [ perc, setPerc ] = useState(0)
-  const [ isDragging, setIsDragging ] = useState(false)
-  const [ initialX, setInitialX ] = useState(0)
+    const [ x, setX ] = useState(0)
+    const [ perc, setPerc ] = useState(0)
+    const [ isDragging, setIsDragging ] = useState(false)
+    const [ initialX, setInitialX ] = useState(0)
+
+    
+    const [ intervalId, setIntervalId ] = useState(null)
+
+    const styles = {
+        outer: {
+            width: "calc(100vw - 136px)",
+            height: 64,
+            padding: 8,
+            borderRadius: 16,
+            background: perc > 0.67 ? "#4CF790" : "#0B0A07",
+            position: "relative",
+        },
+        inner: {
+            height: 64,
+            width: 64,
+            borderRadius: 16,
+            background: "#fff",
+            position: "absolute",
+            left: x + 8,
+            zIndex: 2
+        },
+    }  
+
+    const handleMouseDown = (e) => {
+        setIsDragging(true)
+        setInitialX(e.clientX)
+    }
+
+    const handleTouchStart = (e) => {
+        e.preventDefault()
+        setIsDragging(true)
+        setInitialX(e.touches[0].clientX)
+    }
+
+    const handleMouseUp = (e) => {
+        setIsDragging(false)
+        clearInterval(intervalId)
+        setIntervalId(null)
+        setPerc(x / (window.innerWidth - styles.inner.width - 200))
+
+        if(x >= Math.min((window.innerWidth - 200) / 3 * 2)) {
+            const remainingDistance = Math.min((window.innerWidth - 200)) - x
+            setX(x + remainingDistance)
+            setTimeout(() => {props?.verify(true); console.log("TIME")}, 250)
+            setTimeout(() => {
+                setPerc(0)
+                setX(0)
+                setInitialX(0)
+            }, 251)
+            // props?.verify(true)
+        } else {
+            setX(0)
+        }
+    }
+
+    const handleTouchEnd = (e) => {
+        e.preventDefault()
+        setIsDragging(false)
+        clearInterval(intervalId)
+        setIntervalId(null)
+        setPerc(x / (window.innerWidth - styles.inner.width - 200))
+
+
+        if(x >= Math.min((window.innerWidth - 200) / 2)) {
+            const remainingDistance = Math.min((window.innerWidth - 200)) - x
+            setX(x + remainingDistance)
+            setTimeout(() => {props?.verify(true); console.log("TIME")}, 250)
+            setTimeout(() => {
+                setPerc(0)
+                setX(0)
+                setInitialX(0)
+            }, 251)
+        } else {
+            setX(0)
+        }
+    }
+
+    useEffect(() => {
+        if (isDragging) {
+          window.addEventListener("mousemove", handleMouseMove)
+        }
+
+        return () => {
+          window.removeEventListener("mousemove", handleMouseMove)
+        }
+    }, [isDragging])
+
+    const handleMouseMove = (e) => {
+        const deltaX = e.clientX - initialX
+        setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 200, x + deltaX)))
+        setPerc(x / (window.innerWidth - styles.inner.width - 200))
+    }
+
+    const handleTouchMove = (e) => {
+        const deltaX = e.touches[0].clientX - initialX
+        setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 200, deltaX)))
+        setPerc(x / (window.innerWidth - styles.inner.width - 200))
+    }
 
   
-  const [intervalId, setIntervalId] = useState(null)
+    useEffect(() => {
+        console.log(props)
+        return () => clearInterval(intervalId)
+    }, [])
 
-  const styles = {
-    outer: {
-        width: "calc(100vw - 48px)",
-        height: 64,
-        padding: 8,
-        borderRadius: 16,
-        background: perc > 0.67 ? "#4CF790" : "#0B0A07",
-        position: "relative",
-    },
-    inner: {
-        height: 64,
-        width: 64,
-        borderRadius: 16,
-        background: "#fff",
-        position: "absolute",
-        left: x + 8,
-        zIndex: 2
-    },
-  }  
-
-  const handleMouseDown = (e) => {
-    setIsDragging(true)
-    setInitialX(e.clientX)
+    useEffect(() => {
+        if(props?.verify) {
+            setInitialX(0)
+            setIsDragging(false)
+            setX(0)
+            setPerc(0)
+        }
+    }, [props?.verify])
 
 
-  }
-
-  const handleTouchStart = (e) => {
-    e.preventDefault()
-    setIsDragging(true)
-    setInitialX(e.touches[0].clientX)
-  }
-
-  const handleMouseUp = (e) => {
-    setIsDragging(false)
-    clearInterval(intervalId)
-    setIntervalId(null)
-    setPerc(x / (window.innerWidth - styles.inner.width - 48))
-
-    if(x >= Math.min((window.innerWidth - 96) / 3 * 2)) {
-        const remainingDistance = Math.min((window.innerWidth - 112)) - x
-        setX(x + remainingDistance)
-        props?.verify(true)
-    } else {
-        setX(0)
-    }
-  }
-
-  const handleTouchEnd = (e) => {
-    e.preventDefault()
-    setIsDragging(false)
-    clearInterval(intervalId)
-    setIntervalId(null)
-    setPerc(x / (window.innerWidth - styles.inner.width - 48))
-
-
-    if(x >= Math.min((window.innerWidth - 96) / 2)) {
-        const remainingDistance = Math.min((window.innerWidth - 112)) - x
-        setX(x + remainingDistance)
-        props?.verify(true)
-    } else {
-        setX(0)
-    }
-  }
-
-  useEffect(() => {
-    if (isDragging) {
-      window.addEventListener("mousemove", handleMouseMove)
-    }
-
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove)
-    }
-  }, [isDragging])
-
-  const handleMouseMove = (e) => {
-    const deltaX = e.clientX - initialX
-    console.log(deltaX)
-    setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 48, x + deltaX)))
-    setPerc(x / (window.innerWidth - styles.inner.width - 48))
-  }
-
-  const handleTouchMove = (e) => {
-    const deltaX = e.touches[0].clientX - initialX
-    setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 48, deltaX)))
-    setPerc(x / (window.innerWidth - styles.inner.width - 48))
-  }
-
-  useEffect(() => {
-    console.log(perc, "perc")
-    }, [perc])
-
- 
-  useEffect(() => {
-    return () => clearInterval(intervalId)
-  }, [])
-
-  return (
-    <div style={styles.outer} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-      <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="sliderInner" style={styles.inner}>
-        &nbsp;
-      </div>
-    </div>
-  )
+    return (
+        <div style={styles.outer} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+            <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="sliderInner" style={styles.inner}>
+              &nbsp;
+            </div>
+        </div>
+    )
 }
 
 export default Slider
