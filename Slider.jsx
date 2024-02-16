a85231615e846c42aa279676d3e9e94382824da2
1imo <100065732+1imo@users.noreply.github.com>
2024-02-16 19:58:34 +0000

    Mobile Functionality
    One issue with state changes not happening on click-driven devices for changing of percentage so the colour feedback only occurs on a click up. On mobile devices everything is fine

--- Slider.jsx
+++ Slider.jsx
@@ -15,7 +15,7 @@ function Slider(props) {
         height: 64,
         padding: 8,
         borderRadius: 16,
-        background: perc > 0.32 ? "#0B0A07" : "#4CF790",
+        background: perc > 0.67 ? "#4CF790" : "#0B0A07",
         position: "relative",
     },
     inner: {
@@ -29,31 +29,43 @@ function Slider(props) {
     },
   }  
 
-  useEffect(() => {
-    const position = window.innerWidth - styles.inner.width - 48 - x;
-    // console.log(maxPosition, "MP")
-    setPerc(position / (window.innerWidth - styles.inner.width - 48))
-  }, [x])
-
   const handleMouseDown = (e) => {
     setIsDragging(true)
     setInitialX(e.clientX)
 
-    if (!intervalId) {
-      const newIntervalId = setInterval(() => {
-        // console.log(e)
-      }, 200)
-      setIntervalId(newIntervalId)
-    }
+
   }
 
-  const handleMouseUp = () => {
+  const handleTouchStart = (e) => {
+    e.preventDefault()
+    setIsDragging(true)
+    setInitialX(e.touches[0].clientX)
+  }
+
+  const handleMouseUp = (e) => {
     setIsDragging(false)
     clearInterval(intervalId)
     setIntervalId(null)
+    setPerc(x / (window.innerWidth - styles.inner.width - 48))
 
-    console.log(x, Math.min((window.innerWidth - 96) / 3 * 2))
     if(x >= Math.min((window.innerWidth - 96) / 3 * 2)) {
+        const remainingDistance = Math.min((window.innerWidth - 112)) - x
+        setX(x + remainingDistance)
+        props?.verify(true)
+    } else {
+        setX(0)
+    }
+  }
+
+  const handleTouchEnd = (e) => {
+    e.preventDefault()
+    setIsDragging(false)
+    clearInterval(intervalId)
+    setIntervalId(null)
+    setPerc(x / (window.innerWidth - styles.inner.width - 48))
+
+
+    if(x >= Math.min((window.innerWidth - 96) / 2)) {
         const remainingDistance = Math.min((window.innerWidth - 112)) - x
         setX(x + remainingDistance)
         props?.verify(true)
@@ -74,8 +86,20 @@ function Slider(props) {
 
   const handleMouseMove = (e) => {
     const deltaX = e.clientX - initialX
+    console.log(deltaX)
     setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 48, x + deltaX)))
+    setPerc(x / (window.innerWidth - styles.inner.width - 48))
   }
+
+  const handleTouchMove = (e) => {
+    const deltaX = e.touches[0].clientX - initialX
+    setX(Math.max(0, Math.min(window.innerWidth - styles.inner.width - 48, deltaX)))
+    setPerc(x / (window.innerWidth - styles.inner.width - 48))
+  }
+
+  useEffect(() => {
+    console.log(perc, "perc")
+    }, [perc])
 
  
   useEffect(() => {
@@ -84,7 +108,7 @@ function Slider(props) {
 
   return (
     <div style={styles.outer} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
-      <div onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="sliderInner" style={styles.inner}>
+      <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} className="sliderInner" style={styles.inner}>
         &nbsp;
       </div>
     </div>
